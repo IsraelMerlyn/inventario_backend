eccc1ab28b4ceff891388bedc1c89d1d5618bafe
Israel Merlyn <merlyn142a@gmail.com>
2026-04-02 13:43:56 -0600

    chore: conexion de la BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InventoryModule } from './inventory/inventory.module';
-
+import { InventoryItem } from './inventory/entities/inventory-item.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [InventoryModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root', // Cambia por tus datos
+      password: 'password',
+      database: 'inventory_db',
+      entities: [InventoryItem],
+      synchronize: true, // Auto-crea las tablas (solo para desarrollo) [cite: 25]
+    }),
+    InventoryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
