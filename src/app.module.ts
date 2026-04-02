cda7801421f90179dc8d6fe4975e1408ba4fd650
Israel Merlyn <merlyn142a@gmail.com>
2026-04-02 14:29:41 -0600

    Feat (autenticacion) Estructura de la autenticacion como la creacion del usuario

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,27 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { InventoryModule } from './inventory/inventory.module';
-import { InventoryItem } from './inventory/entities/inventory-item.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module'; // Importa el módulo
+import { InventoryModule } from './inventory/inventory.module'; // Importa el módulo
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity'; // Importa la entidad
+import { InventoryItem } from './inventory/entities/inventory-item.entity'; // Importa la entidad
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'root', // Cambia por tus datos
+      username: 'root',
       password: 'TuPass123!',
       database: 'inventario',
-      entities: [InventoryItem],
-      synchronize: true, // Auto-crea las tablas (solo para desarrollo) [cite: 25]
+      entities: [User, InventoryItem], // Creamos las tablas del Usuario y del Inventario
+      synchronize: true, //  crea las tablas automáticamente
     }),
+    UsersModule,
     InventoryModule,
     AuthModule,
-    UsersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
+import { User } from './entities/user.entity'; // ✅ Importa la entidad
 
 @Module({
-  providers: [UsersService]
+  // Agregamos 
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
